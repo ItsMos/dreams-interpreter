5410ff191250cc3f2417404f8712152a18b93f11
ItsMos <mospro@live.com>
2023-03-29 17:50:09 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <main class="h-screen overflow-x-hidden relative flex flex-col md:flex-row p-10 pb-20 md:pb-10 text-slate-50">
+    <section class="w-[100%] md:w-[50%] flex flex-col justify-center mr-5">
+      <h1 class="text-6xl font-bold bg-gradient-to-r from-[#c0392b] to-[#8e44ad] bg-clip-text text-transparent">Dreams Interpreter</h1>
+      <h3 class="text-lg my-5">Type in your dream below and see what it could mean</h3>
+      <textarea
+        class="bg-white bg-opacity-20 backdrop-blur-sm
+        border-2 border-cyan-200 focus:outline-none focus:border-cyan-500
+        rounded-md p-4 h-52"
+        placeholder="i dreamt of ..."
+        autocomplete="off"></textarea>
+      <button class="bg-orange-500 hover:bg-orange-700 active:scale-95 w-fit mx-auto mt-5 py-3 px-4 rounded-lg text-lg disabled:bg-gray-400">Interpret</button>
+    </section>
+    <section class="w-[100%] md:w-[50%] md:overflow-y-auto
+      border mt-5 md:mt-0 rounded-md p-4
+      bg-slate-900 bg-opacity-40 backdrop-blur-sm
+    ">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia fuga quae ipsam dignissimos tenetur quod, maiores eligendi eveniet voluptate earum necessitatibus? Optio corrupti vitae dolores magni voluptas ipsa consectetur aut?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta velit perferendis id corporis dolore repudiandae aut obcaecati, sequi voluptatem culpa. Ratione quam, illum rerum voluptates laudantium provident aperiam laboriosam omnis?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit consequatur culpa eaque explicabo nisi asperiores a perferendis iste molestias veniam non at, nostrum expedita enim laudantium reiciendis provident quo nesciunt?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia reiciendis at consequatur quisquam tempora quaerat, earum doloribus esse ipsam optio debitis velit provident beatae iste dolorem necessitatibus recusandae ullam minus?
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor fugiat, reiciendis rem officia officiis modi deleniti libero facere, natus accusamus recusandae possimus aliquid enim quae maxime sapiente sunt necessitatibus illum.lo
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate id commodi incidunt autem voluptatum sequi eos, fugit dolores quisquam hic ad aspernatur minima non earum, sed excepturi esse nostrum saepe.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsam magni nihil! Reprehenderit doloribus voluptate perferendis placeat consectetur repellendus illum. Non porro, quae saepe eveniet corrupti id officia aspernatur tenetur?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas rem tempora corrupti consequuntur, tenetur iusto facilis! Similique temporibus itaque placeat rem vero? Veniam dolorem, quas necessitatibus modi totam autem. Sint!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam natus ex dolor ullam quaerat quia aliquid nulla, voluptas sint fugit necessitatibus quam molestiae odit velit! Vitae in animi debitis reprehenderit?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas rem tempora corrupti consequuntur, tenetur iusto facilis! Similique temporibus itaque placeat rem vero? Veniam dolorem, quas necessitatibus modi totam autem. Sint!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam natus ex dolor ullam quaerat quia aliquid nulla, voluptas sint fugit necessitatibus quam molestiae odit velit! Vitae in animi debitis reprehenderit?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas rem tempora corrupti consequuntur, tenetur iusto facilis! Similique temporibus itaque placeat rem vero? Veniam dolorem, quas necessitatibus modi totam autem. Sint!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam natus ex dolor ullam quaerat quia aliquid nulla, voluptas sint fugit necessitatibus quam molestiae odit velit! Vitae in animi debitis reprehenderit?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas rem tempora corrupti consequuntur, tenetur iusto facilis! Similique temporibus itaque placeat rem vero? Veniam dolorem, quas necessitatibus modi totam autem. Sint!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam natus ex dolor ullam quaerat quia aliquid nulla, voluptas sint fugit necessitatibus quam molestiae odit velit! Vitae in animi debitis reprehenderit?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas rem tempora corrupti consequuntur, tenetur iusto facilis! Similique temporibus itaque placeat rem vero? Veniam dolorem, quas necessitatibus modi totam autem. Sint!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam natus ex dolor ullam quaerat quia aliquid nulla, voluptas sint fugit necessitatibus quam molestiae odit velit! Vitae in animi debitis reprehenderit?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas rem tempora corrupti consequuntur, tenetur iusto facilis! Similique temporibus itaque placeat rem vero? Veniam dolorem, quas necessitatibus modi totam autem. Sint!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam natus ex dolor ullam quaerat quia aliquid nulla, voluptas sint fugit necessitatibus quam molestiae odit velit! Vitae in animi debitis reprehenderit?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas rem tempora corrupti consequuntur, tenetur iusto facilis! Similique temporibus itaque placeat rem vero? Veniam dolorem, quas necessitatibus modi totam autem. Sint!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam natus ex dolor ullam quaerat quia aliquid nulla, voluptas sint fugit necessitatibus quam molestiae odit velit! Vitae in animi debitis reprehenderit?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas rem tempora corrupti consequuntur, tenetur iusto facilis! Similique temporibus itaque placeat rem vero? Veniam dolorem, quas necessitatibus modi totam autem. Sint!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam natus ex dolor ullam quaerat quia aliquid nulla, voluptas sint fugit necessitatibus quam molestiae odit velit! Vitae in animi debitis reprehenderit?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas rem tempora corrupti consequuntur, tenetur iusto facilis! Similique temporibus itaque placeat rem vero? Veniam dolorem, quas necessitatibus modi totam autem. Sint!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam natus ex dolor ullam quaerat quia aliquid nulla, voluptas sint fugit necessitatibus quam molestiae odit velit! Vitae in animi debitis reprehenderit?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas rem tempora corrupti consequuntur, tenetur iusto facilis! Similique temporibus itaque placeat rem vero? Veniam dolorem, quas necessitatibus modi totam autem. Sint!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam natus ex dolor ullam quaerat quia aliquid nulla, voluptas sint fugit necessitatibus quam molestiae odit velit! Vitae in animi debitis reprehenderit?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas rem tempora corrupti consequuntur, tenetur iusto facilis! Similique temporibus itaque placeat rem vero? Veniam dolorem, quas necessitatibus modi totam autem. Sint!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam natus ex dolor ullam quaerat quia aliquid nulla, voluptas sint fugit necessitatibus quam molestiae odit velit! Vitae in animi debitis reprehenderit?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas rem tempora corrupti consequuntur, tenetur iusto facilis! Similique temporibus itaque placeat rem vero? Veniam dolorem, quas necessitatibus modi totam autem. Sint!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam natus ex dolor ullam quaerat quia aliquid nulla, voluptas sint fugit necessitatibus quam molestiae odit velit! Vitae in animi debitis reprehenderit?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas rem tempora corrupti consequuntur, tenetur iusto facilis! Similique temporibus itaque placeat rem vero? Veniam dolorem, quas necessitatibus modi totam autem. Sint!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam natus ex dolor ullam quaerat quia aliquid nulla, voluptas sint fugit necessitatibus quam molestiae odit velit! Vitae in animi debitis reprehenderit?
+    </section>
+  </main>
+  <!-- <div class="h-96 -mt-10 bg-gradient-to-b from-[#7c5961] to-transparent">
+
+  </div> -->
+</template>
+
+<script setup>
+</script>
